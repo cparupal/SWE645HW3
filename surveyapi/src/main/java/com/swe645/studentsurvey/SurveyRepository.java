a5fda163263227565c994613934d070a18450d9d
Chetana Parupalli <chetanaparupalli@Chetanas-MBP-2.fios-router.home>
2021-11-06 17:20:25 -0400

    remove surveyapi folder

--- surveyapi/src/main/java/com/swe645/studentsurvey/SurveyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.swe645.studentsurvey;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SurveyRepository extends JpaRepository<SurveyForm, Long>{
-
-}
